e2154a6a04ac45c5fc57530ed097a28b2da144bb
DevHyagooc <hyagooc.dev@gmail.com>
2025-05-02 00:50:12 -0300

    Responsividade sidebar

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -12,6 +12,8 @@ const buttonVariants = cva(
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        cian:
+          "bg-cian text-primary hover:bg-cian/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
@@ -35,7 +37,7 @@ const buttonVariants = cva(
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
+  VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
