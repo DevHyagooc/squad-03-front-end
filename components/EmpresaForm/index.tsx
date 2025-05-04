808de92730146d355f0bc8cf80534102b6cb552f
josealvesdantasjunior@gmail.com <167125176+josealvesdantasjunior@users.noreply.github.com>
2025-05-04 00:20:01 -0300

    Atualização do cadastro de empresa

--- components/EmpresaForm/index.tsx
+++ components/EmpresaForm/index.tsx
@@ -10,21 +10,8 @@ const Form = () => {
     return (
       <form className="space-y-4">
         <div className="flex flex-wrap w-full gap-4 justify-center py-4">
-          <div className="px-2">
-            <label htmlFor="nomeFantasia" className="text-sm font-semibold">
-              Nome Fantasia:
-            </label>
-            <Input id="nomeFantasia" type="text" className="w-52" />
-          </div>
 
-          <div className="px-2">
-            <label htmlFor="razaoSocial" className="text-sm font-semibold">
-              Razão social:
-            </label>
-            <Input id="razaoSocial" type="text" className="w-52" />
-          </div>
-
-          <div className="px-2">
+        <div className="px-2">
             <label htmlFor="cnpj" className="text-sm font-semibold">
               CNPJ:
             </label>
@@ -37,34 +24,44 @@ const Form = () => {
           </div>
 
           <div className="px-2">
+            <label htmlFor="razaoSocial" className="text-sm font-semibold">
+              Razão social:
+            </label>
+            <Input id="razaoSocial" type="text" className="w-52" />
+          </div>
+
+          <div className="px-2">
+            <label htmlFor="nomeFantasia" className="text-sm font-semibold">
+              Nome Fantasia:
+            </label>
+            <Input id="nomeFantasia" type="text" className="w-52" />
+          </div>
+
+          <div className="px-2">
             <label htmlFor="cep" className="text-sm font-semibold">
               CEP:
             </label>
             <Input id="cep" type="text" className="w-52" />
-          </div>
+          </div>          
 
           <div className="px-2">
-            <label htmlFor="email" className="text-sm font-semibold">
-              Email:
-            </label>
-            <Input id="email" type="email" className="w-52" />
-          </div>
-
-          <div className="px-2">
-            <label htmlFor="endereco" className="text-sm font-semibold">
-              Endereço:
+            <label htmlFor="estado" className="text-sm font-semibold">
+              Estado:
             </label>
             <br />
-            <select id="endereco" className="w-52">
-              <option value="avRioBranco">AV Rio Branco</option>
+            <select id="estado" className="w-52">
+              <option value="estado">Rio de Janeiro</option>
             </select>
           </div>
 
           <div className="px-2">
-            <label htmlFor="numero" className="text-sm font-semibold">
-              Número:
+            <label htmlFor="municipio" className="text-sm font-semibold">
+              Município:
             </label>
-            <Input id="numero" type="text" className="w-52" />
+            <br />
+            <select id="municipio" className="w-52">
+              <option value="rioDeJaneiro">Rio de Janeiro</option>
+            </select>
           </div>
 
           <div className="px-2">
@@ -78,13 +75,34 @@ const Form = () => {
           </div>
 
           <div className="px-2">
-            <label htmlFor="municipio" className="text-sm font-semibold">
-              Município:
+            <label htmlFor="endereco" className="text-sm font-semibold">
+              Endereço:
             </label>
             <br />
-            <select id="municipio" className="w-52">
-              <option value="rioDeJaneiro">Rio de Janeiro</option>
+            <select id="endereco" className="w-52">
+              <option value="avRioBranco">AV Rio Branco</option>
             </select>
+          </div>
+
+          <div className="px-2">
+            <label htmlFor="email" className="text-sm font-semibold">
+              Email:
+            </label>
+            <Input id="email" type="email" className="w-52" />
+          </div>
+
+          <div className="px-2">
+            <label htmlFor="inscricaoMunicipal" className="text-sm font-semibold">
+              Inscrição municipal:
+            </label>
+            <Input id="inscricaoMunicipal" type="text" className="w-52" />
+          </div>
+
+          <div className="px-2">
+            <label htmlFor="numero" className="text-sm font-semibold">
+              Número:
+            </label>
+            <Input id="numero" type="text" className="w-52" />
           </div>
 
         </div>
