cf529cfebbb3224a84c801410ee8f543feeeda99
Isaass <isaiasoliver.dev@gmail.com>
2025-05-30 11:55:02 -0300

    feat: upload anexo contrato

--- services/documento.js
+++ services/documento.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8080"
+const BASE_URL = "https://squad-03-back-end.onrender.com"
 const user = "admin"
 const password = "admin123"
 const auth = btoa(`${user}:${password}`)
